e1727e416eb9279d8962d0aaabed38dce7cc6acf
Liam Liden <l14m@msn.com>
2020-04-24 15:35:12 -0500

    Finish movement and add camera control

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject player;
+    public float speed;
+    public Vector3 cameraOffset;
+
+    private Vector3 offset;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        offset = player.transform.position + cameraOffset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Horizontal Rotation
+        Quaternion camAngleHorizontal = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * speed, Vector3.up);
+        
+        // Vertical Rotation
+        Vector3 perpendicular = Vector3.Cross(player.transform.position - transform.position, Vector3.up).normalized;
+        Quaternion camAngleVertical = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * speed, perpendicular);
+
+        // Combine to create orbit
+        offset = camAngleHorizontal * camAngleVertical * offset;
+        transform.position = player.transform.position + offset
+
+        transform.LookAt(player.transform);
+
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,34 +10,91 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody myRB;
     private bool moving;
+    private bool upright;
+    private bool horizontal;
 
     // Start is called before the first frame update
     void Start()
     {
         myRB = GetComponent<Rigidbody>();
+        moving = false;
+        upright = false;
+        horizontal = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        if (moving == false && Input.GetKeyDown(KeyCode.LeftArrow))
+        if (moving == false)
         {
-            StartCoroutine(Move(center.transform.Find("Left").position, Vector3.left));
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                StartCoroutine(Move("Left", Vector3.left));
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                StartCoroutine(Move("Right", Vector3.right));
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                StartCoroutine(Move("Down", Vector3.back));
+            }
+            else if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                StartCoroutine(Move("Up", Vector3.forward));
+            }
         }
     }
 
-    private IEnumerator Move(Vector3 point, Vector3 axis)
+    private IEnumerator Move(string direction, Vector3 axis)
     {
         moving = true;
         for (int i = 0; i < (90 / stepsForRotate); i++)
         {
-            transform.RotateAround(point, axis, stepsForRotate);
+            transform.RotateAround(center.transform.Find(direction).position, axis, stepsForRotate);
             yield return new WaitForSeconds(timeBetweenSteps);
         }
-        center.transform.position = transform.position;
+
+        // Set new pivot locations
+        if (!upright)
+        {
+            if (!horizontal && (direction == "Up" || direction == "Down"))
+            {
+                center.transform.position = transform.position + new Vector3(0, -.5f, 0);
+                center.transform.Find("Down").position += new Vector3(-.5f, 0, 0);
+                center.transform.Find("Up").position += new Vector3(.5f, 0, 0);
+                upright = true;
+            }
+            else if (horizontal && (direction == "Right" || direction == "Left"))
+            {
+                center.transform.position = transform.position + new Vector3(0, -.5f, 0);
+                center.transform.Find("Left").position += new Vector3(0, 0, .5f);
+                center.transform.Find("Right").position += new Vector3(0, 0, -.5f);
+                upright = true;
+            }
+            else
+            {
+                center.transform.position = transform.position;
+            }
+        }
+        else
+        {
+            if (direction == "Up" || direction == "Down")
+            {
+                center.transform.position = transform.position;
+                center.transform.Find("Down").position += new Vector3(.5f, 0, 0);
+                center.transform.Find("Up").position += new Vector3(-.5f, 0, 0);
+                horizontal = false;
+            }
+            else
+            {
+                center.transform.position = transform.position;
+                center.transform.Find("Left").position += new Vector3(0, 0, -.5f);
+                center.transform.Find("Right").position += new Vector3(0, 0, .5f);
+                horizontal = true;
+            }
+            upright = false;
+        }
         moving = false;
     }
 }
