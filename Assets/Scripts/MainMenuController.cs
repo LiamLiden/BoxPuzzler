9c0df767bed61ceaad27e35e929188b03062130d
Liam Liden <l14m@msn.com>
2020-05-06 15:06:42 -0500

    Update MainMenuController.cs

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -7,16 +7,26 @@ public class MainMenuController : MonoBehaviour
 {
     public GameObject levelSelectPanel;
 
+    /// <summary>
+    /// Loads level with same name as input string.
+    /// </summary>
+    /// <param name="level"></param>
     public void LoadLevel(string level)
     {
         SceneManager.LoadScene(level);
     }
 
+    /// <summary>
+    /// Toggles the level select panel.
+    /// </summary>
     public void ToggleLevelSelect()
     {
         levelSelectPanel.SetActive(!levelSelectPanel.activeSelf);
     }
 
+    /// <summary>
+    /// Quits the exe.
+    /// </summary>
     public void Quit()
     {
         Application.Quit();
